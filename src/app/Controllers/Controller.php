5af49696ae5126a2c0ce454fbba6922c3506e5bd
owner <owner@localhost>
2025-11-29 16:44:37 +0900

    コメントなど

--- src/app/Controllers/Controller.php
+++ src/app/Controllers/Controller.php
@@ -16,19 +16,22 @@ abstract class Controller extends BaseController
     /** アクション前処理 */
     public function beforeAction()
     {
+        // トレース
         if (Config::get('debug')) {
             Log::info('Controller: route: ', App::get('router')->currentRoute);
             Log::info('Controller: params: ', $this->params);
         }
 
+        // リダイレクト処理のサンプル
         if ((App::get('router')->currentRoute['options']['name'] ?? null) === 'redirect_test') {
             Log::info('redirect_test');
 
             Location::redirect('/');
 
-            Log::info('after redirect');
+            Log::info('このログは出力されない。');
         }
 
+        // 認証処理
         if ((App::get('router')->currentRoute['options']['auth'] ?? null) === 'user') {
             if (! isset($_SESSION["user_id"])) {
                 Location::redirect('/');
