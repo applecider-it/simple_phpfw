7f7e4208e50ab629142ed34fb0759685b1976436
owner <owner@localhost>
2025-12-02 17:12:05 +0900

    レスポンシブ対応

--- src/resources/views/admin/user/index.html.php
+++ src/resources/views/admin/user/index.html.php
@@ -18,30 +18,32 @@ use SFW\Core\Config;
 
     <?= $this->render('partials.pagination.paginator', ['paginator' => $data['paginator']]) ?>
 
-    <table style="margin-top: 1rem;" class="app-table">
-        <thead>
-            <tr class="app-table-row-header">
-                <th class="app-table-cell" style="text-align: left;">Name</th>
-                <th class="app-table-cell" style="text-align: left;">Email</th>
-                <th class="app-table-cell" style="text-align: left;">削除日時</th>
-                <th class="app-table-cell">更新</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($data['users'] as $user): ?>
-                <tr class="app-table-row-data">
-                    <td class="app-table-cell"><?= Html::esc($user['name'] ?? '') ?></td>
-                    <td class="app-table-cell"><?= Html::esc($user['email'] ?? '') ?></td>
-                    <td class="app-table-cell"><?= $user['deleted_at'] ?? '' ?></td>
-                    <td class="app-table-cell" style="text-align:center;">
-                        <a href="<?= Config::get('adminPrefix') ?>/users/<?= $user['id'] ?>/edit" class="app-btn-primary">
-                            更新
-                        </a>
-                    </td>
+    <div class="app-table-wrap">
+        <table style="margin-top: 1rem;" class="app-table">
+            <thead>
+                <tr class="app-table-row-header">
+                    <th class="app-table-cell" style="text-align: left;">Name</th>
+                    <th class="app-table-cell" style="text-align: left;">Email</th>
+                    <th class="app-table-cell" style="text-align: left;">削除日時</th>
+                    <th class="app-table-cell">更新</th>
                 </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <?php foreach ($data['users'] as $user): ?>
+                    <tr class="app-table-row-data">
+                        <td class="app-table-cell"><?= Html::esc($user['name'] ?? '') ?></td>
+                        <td class="app-table-cell"><?= Html::esc($user['email'] ?? '') ?></td>
+                        <td class="app-table-cell"><?= $user['deleted_at'] ?? '' ?></td>
+                        <td class="app-table-cell" style="text-align:center;">
+                            <a href="<?= Config::get('adminPrefix') ?>/users/<?= $user['id'] ?>/edit" class="app-btn-primary">
+                                更新
+                            </a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
 
     <?= $this->render('partials.pagination.paginator', ['paginator' => $data['paginator']]) ?>
 </div>
