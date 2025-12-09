d8bc951810bf4cd2bd7a0ff855f0b23056e86617
owner <owner@localhost>
2025-12-09 10:59:10 +0900

    開発者向けページフロントエンドテスト

--- src/public/js/services/development/vue/frontend_test_area.js
+++ src/public/js/services/development/vue/frontend_test_area.js
@@ -3,22 +3,48 @@
  */
 const FrontendTestArea = {
   template: `
-  <div style="display:flex; flex-direction:row; gap:1rem;">
-    <button type="submit" class="app-btn-primary" @click="() => execTest('postJsonTest')">
-      Jsonテスト (POST)
-    </button>
+  <div style="display:flex; flex-direction:column; gap:1rem;">
+    <div>
+      <h3>Jsonテスト</h3>
+      <div style="display:flex; flex-direction:row; gap:1rem;">
+        <button type="submit" class="app-btn-primary" @click="() => execTest('postJsonTest')">
+          Jsonテスト (POST)
+        </button>
 
-    <button type="submit" class="app-btn-primary" @click="() => execTest('getJsonTest')">
-      Jsonテスト (GET)
-    </button>
-    
-    <button type="submit" class="app-btn-primary" @click="() => execTest('postNosessionJsonTest')">
-      Jsonテスト (POST nosession)
-    </button>
+        <button type="submit" class="app-btn-primary" @click="() => execTest('getJsonTest')">
+          Jsonテスト (GET)
+        </button>
+        
+        <button type="submit" class="app-btn-primary" @click="() => execTest('postNosessionJsonTest')">
+          Jsonテスト (POST nosession)
+        </button>
+      </div>
+    </div>
+
+    <div>
+      <h3>フォームテスト</h3>
+      <div>
+        <input
+          type="text"
+          class="app-form-input"
+          style="max-width: 30rem;"
+          v-model="val1"
+        >
+      </div>
+      <div>
+        val1: {{ val1 }}
+      </div>
+    </div>
   </div>
   `,
 
   props: ["frontendTest"],
+
+  data() {
+    return {
+      val1: "",
+    };
+  },
 
   methods: {
     /** クリック時 */
