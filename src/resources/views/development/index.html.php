b0e90686d00360a353890c83e4052c0681ca193d
owner <owner@localhost>
2025-12-10 11:10:10 +0900

    開発者向けページ

--- src/resources/views/development/index.html.php
+++ src/resources/views/development/index.html.php
@@ -1,14 +1,25 @@
 <h2 class="app-h2">development.index</h2>
 
-<div>
-    <div><a href="/development/view_test" class="app-link-normal">view_test</a></div>
-    <div><a href="/development/param_test/321?val1=abc" class="app-link-normal">param_test</a></div>
-    <div><a href="/development/database_test" class="app-link-normal">database_test</a></div>
-    <div><a href="/development/validation_test" class="app-link-normal">validation_test</a></div>
-    <div><a href="/development/redirect_test" class="app-link-normal">redirect_test</a></div>
-    <div><a href="/development/exeption_test" class="app-link-normal">exeption_test</a></div>
-    <div><a href="/development/frontend_test" class="app-link-normal">frontend_test</a></div>
-    <div><a href="/development/backend_test" class="app-link-normal">backend_test</a></div>
-    <div><a href="/development/design" class="app-link-normal">design</a></div>
-    <div><a href="/development/notfoud_test" class="app-link-normal">notfoud_test</a></div>
+<div style="display:flex; flex-direction:column; gap:1rem;">
+    <div>
+        <div><a href="/development/frontend_test" class="app-link-normal">frontend_test</a></div>
+        <div><a href="/development/backend_test" class="app-link-normal">backend_test</a></div>
+    </div>
+
+    <div>
+        <div><a href="/development/view_test" class="app-link-normal">view_test</a></div>
+        <div><a href="/development/param_test/321?val1=abc" class="app-link-normal">param_test</a></div>
+        <div><a href="/development/database_test" class="app-link-normal">database_test</a></div>
+        <div><a href="/development/validation_test" class="app-link-normal">validation_test</a></div>
+    </div>
+
+    <div>
+        <div><a href="/development/redirect_test" class="app-link-normal">redirect_test</a></div>
+        <div><a href="/development/exeption_test" class="app-link-normal">exeption_test</a></div>
+        <div><a href="/development/notfoud_test" class="app-link-normal">notfoud_test</a></div>
+    </div>
+
+    <div>
+        <div><a href="/development/design" class="app-link-normal">design</a></div>
+    </div>
 </div>
