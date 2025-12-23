792368360cc0415748080c4e7427098199917402
owner <owner@localhost>
2025-12-23 09:43:57 +0900

    WSエントリーポイント修正

--- ws/app/entrypoints/server.php
+++ ws/app/entrypoints/server.php
@@ -4,7 +4,13 @@ require_once dirname(dirname(__DIR__)) . '/vendor/autoload.php';
 
 use App\Services\WebSocket\Server;
 
-$config = include(dirname(__DIR__) . '/config/config.php');
+/**
+ * WSサーバーエントリーポイント
+ */
 
-$server = new Server($config);
-$server->start();
+(function () {
+    $config = include(dirname(__DIR__) . '/config/config.php');
+
+    $server = new Server($config);
+    $server->start();
+})();
