bf6a06ef163bea0b530f426556e51439f90f9330
owner <owner@localhost>
2025-12-25 10:12:35 +0900

    ベンチマーク

--- src/public/index.php
+++ src/public/index.php
@@ -17,18 +17,22 @@ $endMemory = memory_get_usage();
 $executionTime = $endTime - $startTime;
 $memoryUsed = ($endMemory - $startMemory) / 1024 / 1024; // MB単位
 
+$opcacheStatus = opcache_get_status();
+
 $trace = [
     '処理時間（秒）' => $executionTime,
     'メモリ使用量（MB）' => $memoryUsed,
     'メモリ使用量（MB）開始時' => $startMemory / 1024 / 1024,
     'メモリ使用量（MB）終了時' => $endMemory / 1024 / 1024,
-    'opcache_get_status()' => opcache_get_status(),
+    'opcache使用量（MB）'=> $opcacheStatus['memory_usage']['used_memory'] / 1024 / 1024,
+    'opcache_get_status()' => $opcacheStatus,
 ];
+
 /*
 echo "<div>--------------- performance trace begin ---------------</div>\n";
 echo "<div>performance:</div>\n";
 echo "<pre>\n";
-echo SFW\Output\Html::esc(print_r($trace, true));
+echo SFW\Output\Html::esc(SFW\Data\Json::trace($trace, true));
 echo "</pre>\n";
 echo "<div>--------------- performance trace end ---------------</div>\n";
 */
