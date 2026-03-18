ff45d73f27e9c2e7138d243e4187aeca0c5385a1
owner <owner@localhost>
2026-03-18 11:20:33 +0900

    コメント

--- src/simple-phpfw/src/Output/View/Template.php
+++ src/simple-phpfw/src/Output/View/Template.php
@@ -33,7 +33,17 @@ class Template
         $this->templates['sfw.blade'] = new SfwTemplate;
     }
 
-    /** 描画情報を返す */
+    /**
+     * 描画情報を返す
+     * 
+     * テンプレートがないときは例外
+     * 
+     * 戻り値  
+     * $typeが`html`の時  
+     * [name => string, baseDir => string, type => string, path => string,]  
+     * それ以外  
+     * [name => string, baseDir => string, type => string, path => string, srcPath => string]  
+     */
     public function renderInfo(string $name): array
     {
         $baseDir = $this->view->baseDir() ?? SFW_PROJECT_ROOT . '/resources/views';
@@ -48,7 +58,18 @@ class Template
         return $meta;
     }
 
-    /** テンプレートを探す */
+    /**
+     * テンプレートを探す
+     * 
+     * 戻り値
+     * 
+     * テンプレートがないときはnull
+     * 
+     * $typeが`html`の時  
+     * [type => string, path => string,]  
+     * それ以外  
+     * [type => string, path => string, srcPath => string]  
+     */
     private function findTemplate(string $baseDir, string $name): ?array
     {
         foreach (self::FILE_TYPES as $type) {
@@ -61,8 +82,17 @@ class Template
         return null;
     }
 
-    /** パス情報を返す */
-    private function getPathInfo(string $path, string $type): ?array
+    /**
+     * パス情報を返す
+     * 
+     * 戻り値
+     * 
+     * $typeが`html`の時  
+     * [path => string,]  
+     * それ以外  
+     * [path => string, srcPath => string]  
+     */
+    private function getPathInfo(string $path, string $type): array
     {
         if ($type !== 'html') {
             return $this->getGenartedPathInfo($path, $type);
@@ -71,7 +101,11 @@ class Template
         return compact('path');
     }
 
-    /** テンポラリーファイルなどを含んだパス情報を返す */
+    /**
+     * テンポラリーファイルなどを含んだパス情報を返す
+     * 
+     * 必要な時は、テンポラリーファイルを生成する。
+     */
     private function getGenartedPathInfo(string $path, string $type): ?array
     {
         /** @var string テンポラリーファイルパス */
@@ -82,6 +116,7 @@ class Template
         if ($needGenerate) {
             // テンポラリーファイル生成が必要な時
 
+            // テンポラリーファイル生成
             $templateData = file_get_contents($path);
             $resultTemplateData = $this->templates[$type]->convertTemplate($templateData);
             file_put_contents($tmpPath, $resultTemplateData);
@@ -90,12 +125,12 @@ class Template
         }
 
         return [
-            'path' => $tmpPath,
+            'path' => $tmpPath, // Viewクラスから読み込まれるのはテンポラリーファイルのほう
             'srcPath' => $path,
         ];
     }
 
-    /** 生成が必要か返す */
+    /** テンポラリーファイル生成が必要か返す */
     private function checkGenarate(string $path, string $tmpPath): bool
     {
         if (!file_exists($tmpPath)) {
