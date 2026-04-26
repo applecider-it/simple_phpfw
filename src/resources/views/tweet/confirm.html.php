433b5cef8b98f20ae2403df6bf19426cf6e9634c
owner <owner@localhost>
2026-04-26 12:17:24 +0900

    vite

--- src/resources/views/tweet/confirm.html.php
+++ src/resources/views/tweet/confirm.html.php
@@ -5,14 +5,14 @@
         <?= $this->render('partials.form.csrf') ?>
 
         <div style="margin-top: 1rem;">
-            <label>内容</label>
-            <?= $this->h($content ?? '') ?>
+            <label class="app-form-label">内容</label>
+            <div class="h-10"><?= $this->h($content ?? '') ?></div>
             <input type="hidden" name="content" value="<?= $this->h($content ?? '') ?>">
         </div>
 
         <div style="margin-top: 1rem;">
-            <button type="submit" name="back" value="on">戻る</button>
-            <button type="submit" name="commit" value="on">投稿確定</button>
+            <button type="submit" name="back" value="on" class="app-btn-secondary">戻る</button>
+            <button type="submit" name="commit" value="on" class="app-btn-primary">投稿確定</button>
         </div>
     </form>
 </div>
